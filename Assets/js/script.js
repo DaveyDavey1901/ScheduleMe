f0372d736657fa754c32b9fa60ea87cf3c0f6a5d
David Davies <David_Davies@mail.com>
2021-11-15 21:28:24 +0000

    added a function to check each textarea and change the class to change the color depening on time.

---
+++ Assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  // display current day, year and time ontop of the page
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+  //gets the current time in this case 24hour clock with kk
+  var now = parseInt(moment().format("kk"));
+//looks at the testarea name and compares it to the current time. 
+// then adds the relevenat class to change the color of the cells.past grey, present red, and future green.
+  
+  $("textarea").each(function () {
+    var name = parseInt($(this).attr("name"));
+    console.log(name);
+    if (name < now) {
+      $(this).addClass("past");
+    }
+
+    if (name > now) {
+      $(this).addClass("future");
+    }
+    
+    if(name === now) {
+      $(this).addClass("present");
+    }
+  });
+
+  
+});
