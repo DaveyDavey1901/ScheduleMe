b1e043e326af3885f9dedc89dc95a5bb7db4f823
David Davies <David_Davies@mail.com>
2021-11-15 23:44:53 +0000

    added the abilty to save to local storage... unsure how to get it back out at the moment.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
   //gets the current time in this case 24hour clock with kk
   var now = parseInt(moment().format("kk"));
-//looks at the testarea name and compares it to the current time. 
-// then adds the relevenat class to change the color of the cells.past grey, present red, and future green.
-  
+  //looks at the testarea name and compares it to the current time.
+  // then adds the relevenat class to change the color of the cells.past grey, present red, and future green.
+
   $("textarea").each(function () {
     var name = parseInt($(this).attr("name"));
     console.log(name);
@@ -16,11 +16,18 @@ $(document).ready(function () {
     if (name > now) {
       $(this).addClass("future");
     }
-    
-    if(name === now) {
+
+    if (name === now) {
       $(this).addClass("present");
     }
   });
 
-  
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var textEntry = $(this).siblings(".description").val();
+    var entryTime = $(this).parent().attr("id");
+
+    localStorage.setItem(entryTime, JSON.stringify(textEntry));
+  });
 });
+
