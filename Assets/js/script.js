4c7fbda76cd3a0255d0d1873642b0b1340e9827b
David Davies <David_Davies@mail.com>
2021-11-16 00:41:50 +0000

    enabled the var now for correct time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // display current day, year and time ontop of the page
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
   //gets the current time in this case 24hour clock with kk
-  var now = 13//parseInt(moment().format("kk"));
+  var now = parseInt(moment().format("kk"));
 
   //looks at the testarea name and compares it to the current time.
   // then adds the relevent class to change the color of the cells.past grey, present red, and future green.
